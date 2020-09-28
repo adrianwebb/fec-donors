a8f4ae14b6c059ae0cbdef8cc4070efa728d745d
Adrian Webb <adrian@webb.sh>
2020-09-28 03:15:02 -0400

    Updating the receipt source provider to process committee donations in sequence.

--- plugins/source/receipt.py
+++ plugins/source/receipt.py
@@ -7,40 +7,43 @@ import pandas
 
 class Provider(BaseProvider('source', 'receipt')):
 
-    def load(self):
+    def update(self):
         committee_facade = self.facade_index['committee']
-        committees = {}
-        data = []
 
         for committee_id in list(committee_facade.field_values('committee_id')):
-            committees[committee_id] = True
+            data = self.load(committee_id)
+            if data is not None:
+                self.save(self.validate(data))
 
+
+    def load(self, committee_id):
+        data = []
         receipts = ScheduleATransaction.fetch(
+            committee_id = [ committee_id ],
             two_year_transaction_period = self.field_year,
-            sort_null_only = True,
             is_individual =  True,
+            sort_null_only = True,
             per_page = 100
         )
         for receipt in receipts:
-            if receipt.committee_id and receipt.committee_id in committees:
-                data.append([
-                    receipt.transaction_id if receipt.transaction_id else receipt.sub_id,
-                    receipt.report_year,
-                    receipt.committee_id,
-                    receipt.candidate_id,
-                    receipt.election_type,
-                    receipt.contribution_receipt_amount,
-                    receipt.contributor_first_name.strip().title() if receipt.contributor_first_name else None,
-                    receipt.contributor_middle_name.strip().title() if receipt.contributor_middle_name else None,
-                    receipt.contributor_last_name.strip().title() if receipt.contributor_last_name else None,
-                    receipt.contributor_street_1.strip().title() if receipt.contributor_street_1 else None,
-                    receipt.contributor_street_2.strip().title() if receipt.contributor_street_2 else None,
-                    receipt.contributor_city.strip().title() if receipt.contributor_city else None,
-                    receipt.contributor_state,
-                    receipt.contributor_zip,
-                    receipt.contributor_employer.strip().title() if receipt.contributor_employer else None,
-                    receipt.contributor_occupation.strip().title() if receipt.contributor_occupation else None
-                ])
+            data.append([
+                receipt.transaction_id if receipt.transaction_id else receipt.sub_id,
+                receipt.report_year,
+                receipt.committee_id,
+                receipt.candidate_id,
+                receipt.election_type,
+                receipt.contribution_receipt_amount,
+                receipt.contributor_first_name.strip().title() if receipt.contributor_first_name else None,
+                receipt.contributor_middle_name.strip().title() if receipt.contributor_middle_name else None,
+                receipt.contributor_last_name.strip().title() if receipt.contributor_last_name else None,
+                receipt.contributor_street_1.strip().title() if receipt.contributor_street_1 else None,
+                receipt.contributor_street_2.strip().title() if receipt.contributor_street_2 else None,
+                receipt.contributor_city.strip().title() if receipt.contributor_city else None,
+                receipt.contributor_state,
+                receipt.contributor_zip,
+                receipt.contributor_employer.strip().title() if receipt.contributor_employer else None,
+                receipt.contributor_occupation.strip().title() if receipt.contributor_occupation else None
+            ])
 
         return pandas.DataFrame(data, columns = [
             'transaction_id',
