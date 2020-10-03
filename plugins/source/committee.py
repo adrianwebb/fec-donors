1bdce609074abba48d43b4c8dc44bad9b487a2f4
Adrian Webb <adrian@webb.sh>
2020-10-03 05:35:04 -0400

    Adding candidate filtering to the committee source provider and failure conditional processing in the committee and receipt source providers.

--- plugins/source/committee.py
+++ plugins/source/committee.py
@@ -7,34 +7,61 @@ import pandas
 
 class Provider(BaseProvider('source', 'committee')):
 
-    def load(self):
-        data = []
-        committees = Committee.fetch(
-            year = self.field_year,
-            q = self.field_committees,
-            committee_type = self.field_type_code,
-            party = [ self.field_party_code ],
-            per_page = 100
-        )
-        for committee in committees:
-            data.append([
-                committee.committee_id,
-                committee.name.strip().title(),
-                committee.committee_type_full,
-                committee.designation_full,
-                committee.filing_frequency,
-                committee.state,
-                committee.party_full,
-                ",".join(committee.candidate_ids)
-            ])
+    page_count = 100
+    columns = [
+        'committee_id',
+        'name',
+        'type',
+        'designation',
+        'filing_frequency',
+        'state',
+        'party',
+        'candidate_ids'
+    ]
 
-        return pandas.DataFrame(data, columns = [
-            'committee_id',
-            'name',
-            'type',
-            'designation',
-            'filing_frequency',
-            'state',
-            'party',
-            'candidate_ids'
-        ])
+    def update(self):
+        candidate_facade = self.facade_index['candidate']
+        data = []
+
+        next_candidate_id = self.command.get_state('committee_import_next')
+        process = False if next_candidate_id else True
+
+        for candidate_id in list(candidate_facade.field_values('candidate_id')):
+            if next_candidate_id and candidate_id == next_candidate_id:
+                process = True
+
+            if process:
+                self.command.set_state('committee_import_next', candidate_id)
+
+                committees = Committee.fetch(
+                    candidate_id = [ candidate_id ],
+                    year = self.field_year,
+                    q = self.field_committees,
+                    committee_type = self.field_type_code,
+                    party = [ self.field_party_code ],
+                    per_page = self.page_count
+                )
+                for committee in committees:
+                    data.append([
+                        committee.committee_id,
+                        committee.name.strip().title(),
+                        committee.committee_type_full,
+                        committee.designation_full,
+                        committee.filing_frequency,
+                        committee.state,
+                        committee.party_full,
+                        ",".join(committee.candidate_ids)
+                    ])
+                    if len(data) >= self.page_count:
+                        data = self._save(data)
+
+            self._save(data)
+            self.command.delete_state('committee_import_next')
+
+
+    def _save(self, data):
+        if data:
+            records = pandas.DataFrame(data, columns = self.columns)
+            self.save(self.validate(records))
+            data = []
+        return data
--- plugins/source/receipt.py
+++ plugins/source/receipt.py
@@ -28,40 +28,51 @@ class Provider(BaseProvider('source', 'receipt')):
     ]
 
     def update(self):
+        state_facade = self.facade_index['state']
         committee_facade = self.facade_index['committee']
         data = []
 
+        next_committee_id = self.command.get_state('receipt_import_next')
+        process = False if next_committee_id else True
+
         for committee_id in list(committee_facade.field_values('committee_id')):
-            receipts = ScheduleATransaction.fetch(
-                committee_id = [ committee_id ],
-                two_year_transaction_period = self.field_year,
-                is_individual =  True,
-                sort_null_only = True,
-                per_page = self.page_count
-            )
-            for receipt in receipts:
-                data.append([
-                    receipt.transaction_id if receipt.transaction_id else receipt.sub_id,
-                    receipt.report_year,
-                    receipt.committee_id,
-                    receipt.candidate_id,
-                    receipt.election_type,
-                    receipt.contribution_receipt_amount,
-                    receipt.contributor_first_name.strip().title() if receipt.contributor_first_name else None,
-                    receipt.contributor_middle_name.strip().title() if receipt.contributor_middle_name else None,
-                    receipt.contributor_last_name.strip().title() if receipt.contributor_last_name else None,
-                    receipt.contributor_street_1.strip().title() if receipt.contributor_street_1 else None,
-                    receipt.contributor_street_2.strip().title() if receipt.contributor_street_2 else None,
-                    receipt.contributor_city.strip().title() if receipt.contributor_city else None,
-                    receipt.contributor_state,
-                    receipt.contributor_zip,
-                    receipt.contributor_employer.strip().title() if receipt.contributor_employer else None,
-                    receipt.contributor_occupation.strip().title() if receipt.contributor_occupation else None
-                ])
-                if len(data) == self.page_count:
-                    data = self._save(data)
+            if next_committee_id and committee_id == next_committee_id:
+                process = True
+
+            if process:
+                self.command.set_state('receipt_import_next', committee_id)
+
+                receipts = ScheduleATransaction.fetch(
+                    committee_id = [ committee_id ],
+                    two_year_transaction_period = self.field_year,
+                    is_individual =  True,
+                    sort_null_only = True,
+                    per_page = self.page_count
+                )
+                for receipt in receipts:
+                    data.append([
+                        receipt.transaction_id if receipt.transaction_id else receipt.sub_id,
+                        receipt.report_year,
+                        receipt.committee_id,
+                        receipt.candidate_id,
+                        receipt.election_type,
+                        receipt.contribution_receipt_amount,
+                        receipt.contributor_first_name.strip().title() if receipt.contributor_first_name else None,
+                        receipt.contributor_middle_name.strip().title() if receipt.contributor_middle_name else None,
+                        receipt.contributor_last_name.strip().title() if receipt.contributor_last_name else None,
+                        receipt.contributor_street_1.strip().title() if receipt.contributor_street_1 else None,
+                        receipt.contributor_street_2.strip().title() if receipt.contributor_street_2 else None,
+                        receipt.contributor_city.strip().title() if receipt.contributor_city else None,
+                        receipt.contributor_state,
+                        receipt.contributor_zip,
+                        receipt.contributor_employer.strip().title() if receipt.contributor_employer else None,
+                        receipt.contributor_occupation.strip().title() if receipt.contributor_occupation else None
+                    ])
+                    if len(data) >= self.page_count:
+                        data = self._save(data)
 
             self._save(data)
+            self.command.delete_state('receipt_import_next')
 
 
     def _save(self, data):
