e852198948af2061f23c27bff26cb3b0567427f1
Adrian Webb <adrian@webb.sh>
2020-11-01 08:21:04 -0500

    Refactoring the candidate source provider to use plugin mixin methods and the utility FEC API class for loading data.

--- plugins/source/candidate.py
+++ plugins/source/candidate.py
@@ -1,131 +1,98 @@
-from pyopenfec import Candidate
-
 from systems.plugins.index import BaseProvider
-from utility.data import ensure_list, get_identifier
-
-import os
+from utility.fec_api import OpenFECAPI
 
 
 class Provider(BaseProvider('source', 'candidate')):
 
     def item_columns(self):
-        return {
-            'year': [
-                'year'
-            ],
-            'political_office': [
-                'office_id',
-                'office_name'
-            ],
-            'political_party': [
-                'party_id',
-                'party_name'
-            ],
-            'us_state': [
-                'state_id'
-            ],
-            'us_district': [
-                'district_id',
-                'state_id',
-                'district_number'
-            ],
-            'candidate': [
-                'candidate_id',
-                'first_name',
-                'last_name',
-                'office_id',
-                'party_id'
-            ],
-            'candidacy': [
-                'candidacy_id',
-                'candidate_id',
-                'district_id',
-                'year'
-            ]
-        }
+        return [
+            'year',
+            'office_id',
+            'party_id',
+            'state_id',
+            'district_id',
+            'district_number',
+            'candidate_id',
+            'first_name',
+            'last_name',
+            'candidacy_id'
+        ]
 
-    def load_contexts(self):
-        return ensure_list(self.field_years)
+    def load_items(self, context):
+        return OpenFECCandidates(
+            self.command,
+            self.field_year,
+            self.page_count
+        ).fetch()
 
-    def load_items(self, year):
-        return Candidate.fetch(
-            cycle = [ year ],
-            name = ensure_list(self.field_candidates, True),
-            office = ensure_list(self.field_office_codes, True),
-            party = ensure_list(self.field_party_codes, True),
-            per_page = self.page_count
-        )
-
-    def load_item(self, candidate, year):
+    def load_item(self, candidate, context):
         name_components = self._normalize_name_components(candidate.name)
         if name_components and len(name_components) >= 2:
-            cycle_info = self._find_most_recent_year(candidate.cycles, year)
-            election_info = self._find_most_recent_year(candidate.election_years, year)
+            election_info = self._find_most_recent_year(candidate.election_years, self.field_year)
+            district_number = candidate.election_districts[election_info['index']]
 
-            try:
-                district_number = int(candidate.election_districts[election_info['index']])
-            except Exception:
-                district_number = None
-
-            if district_number != 0:
-                district_id = "{}{}".format(candidate.state, str(district_number))
-            else:
-                district_id = candidate.state
-
-            candidacy_id = get_identifier([
+            return [
+                election_info['year'],
+                candidate.office,
+                candidate.party,
+                candidate.state,
+                self._get_district_id(candidate.state, district_number),
+                district_number,
                 candidate.candidate_id,
-                district_id,
-                candidate.office_full,
-                str(election_info['year'])
-            ])
-            return {
-                'year': [
-                    [ cycle_info['year'] ],
-                    [ election_info['year'] ]
-                ],
-                'political_office': [
-                    candidate.office,
-                    candidate.office_full.title()
-                ],
-                'political_party': [
-                    candidate.party,
-                    candidate.party_full.title()
-                ],
-                'us_state': [
-                    candidate.state
-                ],
-                'us_district': [
-                    district_id,
+                self._format_name(name_components[1].split()[0]),
+                self._format_name(name_components[0]),
+                self._get_candidacy_id(
+                    candidate.candidate_id,
                     candidate.state,
-                    district_number
-                ],
-                'candidate': [
-                    candidate.candidate_id,
-                    self._format_name(name_components[1].split()[0]),
-                    self._format_name(name_components[0]),
+                    district_number,
                     candidate.office,
-                    candidate.party
-                ],
-                'candidacy': [
-                    candidacy_id,
-                    candidate.candidate_id,
-                    district_id,
                     election_info['year']
-                ]
-            }
+                )
+            ]
         return None
 
 
-    def _normalize_name_components(self, name):
-        name_components = name.split(",")
-        final_components = []
+class OpenFECCandidates(OpenFECAPI):
 
-        if len(name_components) == 1:
-            name_components = name.split()
-            name_components = [name_components[-1], name_components[0]]
+    @property
+    def state_id(self):
+        return "candidate-index-{}".format(self.year)
 
-        for component in name_components:
-            if component:
-                final_components.append(component)
 
-        return final_components
+    def fetch(self):
+        endpoint = 'candidates'
+        options = {}
+        next_page = None
+
+        page = self.command.get_state(self.state_id, None)
+        if page:
+            options['page'] = page
+
+        options['cycle'] = [ self.year ]
+        options['sort'] = 'name'
+        initial_results = self.request(endpoint, options)
+
+        if initial_results.get('results', None) and len(initial_results['results']) > 0:
+            for result in initial_results['results']:
+                yield type('Candidate', (object,), result)
+
+        if initial_results.get('pagination', None):
+            if initial_results['pagination'].get('pages', None):
+                if initial_results['pagination']['pages'] > 1:
+                    next_page = page + 1 if page else 2
+                    self.command.set_state(self.state_id, next_page)
+
+        if next_page:
+            while next_page <= initial_results['pagination']['pages']:
+                params = dict(options)
+                params['page'] = next_page
+                paged_results = self.request(endpoint, params)
+
+                if paged_results.get('results', None) and len(paged_results['results']) > 0:
+                    for result in paged_results['results']:
+                        yield type('Candidate', (object,), result)
+
+                next_page += 1
+                self.command.set_state(self.state_id, next_page)
+
+        self.command.delete_state(self.state_id)
